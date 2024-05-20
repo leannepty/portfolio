84de3cc84ba73e6efc3079fb33194aa63379067d
Léanne PAUTY <leanne.pauty@jvs.fr>
2024-05-20 18:04:06 +0000

    refactor: style AboutMe

--- src/component/Infos.tsx
+++ src/component/Infos.tsx
@@ -7,35 +7,26 @@ import { Cursor, useTypewriter } from 'react-simple-typewriter'
 
 export const Infos = () => {
   const [text] = useTypewriter({
-    words: ['Hello world !', 'Je suis Léanne Pauty'],
+    words: ['Hello world !', 'Je suis Léanne Pauty :)'],
     loop: 5
   })
 
   return (
-    <Card >
-      <CardBody>
-        <Flex>
-          <Stack m='6' spacing={8}>
-            <Heading size='lg'>{text} <Cursor /></Heading>
-            <Text>
-              Je m&apos;appelle  Léanne PAUTY, je suis étudiante en troisème année d&apos;informatique à l&apos;IUT de Reims.
+    <Flex>
+      <Stack m='6' spacing={8}>
+        <Heading size='lg'>{text} <Cursor /></Heading>
+        <Text>
+          Je m&apos;appelle  Léanne PAUTY, je suis étudiante en troisème année d&apos;informatique à l&apos;IUT de Reims.
 
-            </Text>
-            <Text>
-              Ma formation ma permis d&apos;acquérir les bases de l&apos;informatique : je maîtrise notamment les langages python, Javascript, PHP, HTML, CSS et SQL avec l&apos;utilisation de Git, Oracle ainsi que divers framework backend comme frontend.
+        </Text>
+        <Text>
+          Ma formation ma permis d&apos;acquérir les bases de l&apos;informatique : je maîtrise notamment les langages python, Javascript, PHP, HTML, CSS et SQL avec l&apos;utilisation de Git, Oracle ainsi que divers framework backend comme frontend.
 
-            </Text>
+        </Text>
 
-            <Text>
-              J&apos;ai effectué ma seconde année de BUT informatique au sein du parcours Data ce qui m&apos;a permis de maîtriser davantage la gestion des données et leur exploitation en parallèle des enseignements de développement. Cependant ayant plus d&apos;appétence pour le développement, j&apos;ai effectué les démarches afin de rejoindre le parcours développement pour ma 3ème année.
-            </Text>
-          </Stack>
-          <Image
-            boxSize='sm' src='img/myself.png' alt='PAUTY Léanne photographie' borderRadius={"full"} />
-        </Flex>
-      </CardBody>
-      <Divider />
-      <CardFooter>
+        <Text>
+          J&apos;ai effectué ma seconde année de BUT informatique au sein du parcours Data ce qui m&apos;a permis de maîtriser davantage la gestion des données et leur exploitation en parallèle des enseignements de développement. Cependant ayant plus d&apos;appétence pour le développement, j&apos;ai effectué les démarches afin de rejoindre le parcours développement pour ma 3ème année.
+        </Text>
         <Flex gap={2}>
           <Link href='https://www.linkedin.com/in/l%C3%A9anne-pauty-488593227/' >
             <LinkedinIcon />
@@ -47,7 +38,10 @@ export const Infos = () => {
             <CVIcon />
           </Link>
         </Flex>
-      </CardFooter>
-    </Card>
+      </Stack>
+      <Image
+        boxSize='sm' src='img/myself.png' alt='PAUTY Léanne photographie' borderRadius={"full"} />
+    </Flex>
+
   )
 }
--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -2,6 +2,9 @@ import { extendTheme } from "@chakra-ui/react";
 
 const colors = {
   primary: {
+    200: "#E1EFDA",
+    300: "#CCE4C0",
+    400: "#AAC69B",
     500: "#7BA465",
   }
 };
