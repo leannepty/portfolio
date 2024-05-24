b928cbe88aeb79452213f8969aef6a111d80be9c
Léanne PAUTY <leanne.pauty@jvs.fr>
2024-05-24 05:42:42 +0000

    feat: Ajout logo redux

--- src/component/constants.tsx
+++ src/component/constants.tsx
@@ -28,58 +28,60 @@ import { RasberryPi } from "./logo/RaspberryPi"
 import { Docker } from "./logo/Docker"
 import { Apache } from "./logo/Apache"
 import { DBeaver } from "./logo/DBeaver"
+import { Redux } from "./logo/Redux"
 
 const skills: { theme: string, content: { nom: string, logo: React.JSX.Element }[] }[] = [
   {
     theme: 'Langages', content: [
-      { nom: 'Typescript', logo: <Typescript/> },
-      { nom: 'Python', logo: <Python/> },
-      { nom: 'HTML', logo: <HTML/> },
-      { nom: 'CSS', logo: <CSS/> },
-      { nom: 'PHP', logo: <PHP/> },
-      { nom: 'SQL', logo: <SQL/> }
+      { nom: 'Typescript', logo: <Typescript /> },
+      { nom: 'Python', logo: <Python /> },
+      { nom: 'HTML', logo: <HTML /> },
+      { nom: 'CSS', logo: <CSS /> },
+      { nom: 'PHP', logo: <PHP /> },
+      { nom: 'SQL', logo: <SQL /> }
     ]
   },
   {
     theme: 'Frameworks', content: [
-      { nom: 'React', logo: <ReactLogo/> },
-      { nom: 'React Native', logo: <ReactNative/> },
-      { nom: 'Flask', logo: <Flask/> },
-      { nom: 'Express JS', logo: <Express/> },
-      { nom: 'Next JS', logo: <Next/> },
-      { nom: 'Nest JS', logo: <Nest/> },
-      { nom: 'Symfony', logo: <Symfony/> },
-      { nom: 'API Platform', logo: <APIPlatform/>}
+      { nom: 'React', logo: <ReactLogo /> },
+      { nom: 'React Native', logo: <ReactNative /> },
+      { nom: 'Flask', logo: <Flask /> },
+      { nom: 'Express JS', logo: <Express /> },
+      { nom: 'Next JS', logo: <Next /> },
+      { nom: 'Nest JS', logo: <Nest /> },
+      { nom: 'Symfony', logo: <Symfony /> },
+      { nom: 'API Platform', logo: <APIPlatform /> }
     ]
   },
   {
     theme: 'Applications', content: [
-      { nom: 'Gitlab', logo: <Gitlab/> },
-      { nom: 'DBeaver', logo:<DBeaver/> },
-      { nom: 'Github', logo: <GitHub/> },
-      { nom: 'VSCode', logo: <VSCode/> },
-      { nom: 'Figma', logo: <Figma/> },
-      { nom: 'Jira', logo: <Jira/> },
-      { nom: 'PowerBI', logo: <PowerBi/> },
-      { nom: 'Packet Tracer', logo: <PacketTracer/>},
-      { nom: 'PHPMyAdmin', logo: <PhpMyAdmin/> }
+      { nom: 'Gitlab', logo: <Gitlab /> },
+      { nom: 'DBeaver', logo: <DBeaver /> },
+      { nom: 'Github', logo: <GitHub /> },
+      { nom: 'VSCode', logo: <VSCode /> },
+      { nom: 'Figma', logo: <Figma /> },
+      { nom: 'Jira', logo: <Jira /> },
+      { nom: 'PowerBI', logo: <PowerBi /> },
+      { nom: 'Packet Tracer', logo: <PacketTracer /> },
+      { nom: 'PHPMyAdmin', logo: <PhpMyAdmin /> }
     ]
   },
   {
     theme: "Système d'exploitation", content: [
-      { nom: 'Windows', logo: <Windows/> },
-      { nom: 'Debian', logo: <Debian/> },
-      { nom: 'Ubuntu', logo: <Ubuntu/> },
-      { nom: 'RasberryPi', logo: <RasberryPi/> },
+      { nom: 'Windows', logo: <Windows /> },
+      { nom: 'Debian', logo: <Debian /> },
+      { nom: 'Ubuntu', logo: <Ubuntu /> },
+      { nom: 'RasberryPi', logo: <RasberryPi /> },
     ]
   },
   {
     theme: "Autres", content: [
-      { nom: 'Docker', logo: <Docker/> },
-      { nom: 'Apache', logo: <Apache/> }
+      { nom: 'Docker', logo: <Docker /> },
+      { nom: 'Apache', logo: <Apache /> },
+      { nom: 'Redux', logo: <Redux /> }
     ]
   }
-  
+
 ]
 
-export {skills}
+export { skills }
